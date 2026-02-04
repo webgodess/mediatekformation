ac5bc26743b1e326fd5d03650aa5a54ce62d84dc
webby! <sritarbrt@gmail.com>
2026-02-04 19:36:23 +0100

    fix: changement de utf8 pour corriger erreur dans mysql

--- mediatekformation.sql
+++ mediatekformation.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `mediatekformation`
 --
-CREATE DATABASE IF NOT EXISTS `mediatekformation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `mediatekformation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `mediatekformation`;
 
 -- --------------------------------------------------------
