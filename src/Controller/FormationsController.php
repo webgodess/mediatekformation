3c11898dc6136c77b71aa3d44c15ddccfe581c23
webby! <sritarbrt@gmail.com>
2026-02-05 18:23:26 +0100

    refactor: définition d'une constante pour le chemin des formations

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -27,16 +27,19 @@ class FormationsController extends AbstractController {
      */
     private $categorieRepository;
 
+    const RENDER_PATH = "pages/formations.html.twig";
+
     public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
         $this->formationRepository = $formationRepository;
         $this->categorieRepository = $categorieRepository;
     }
+    
 
     #[Route('/formations', name: 'formations')]
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::RENDER_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -46,7 +49,7 @@ class FormationsController extends AbstractController {
     public function sort($champ, $ordre, $table=""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::RENDER_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -57,7 +60,7 @@ class FormationsController extends AbstractController {
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::RENDER_PATH, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
@@ -68,7 +71,7 @@ class FormationsController extends AbstractController {
     #[Route('/formations/formation/{id}', name: 'formations.showone')]
     public function showOne($id): Response{
         $formation = $this->formationRepository->find($id);
-        return $this->render("pages/formation.html.twig", [
+        return $this->render(self::RENDER_PATH, [
             'formation' => $formation
         ]);
     }
