87f8a430bbf626e3afe788d1977b4ada9a54129a
webby! <sritarbrt@gmail.com>
2026-02-05 22:14:19 +0100

    refactor: fusion de deux conditions if

--- src/Entity/Playlist.php
+++ src/Entity/Playlist.php
@@ -82,11 +82,9 @@ class Playlist
 
     public function removeFormation(Formation $formation): static
     {
-        if ($this->formations->removeElement($formation)) {
+        if ($this->formations->removeElement($formation) && $formation->getPlaylist() === $this) {
             // set the owning side to null (unless already changed)
-            if ($formation->getPlaylist() === $this) {
-                $formation->setPlaylist(null);
-            }
+            $formation->setPlaylist(null);
         }
 
         return $this;
@@ -100,9 +98,10 @@ class Playlist
         $categories = new ArrayCollection();
         foreach($this->formations as $formation){
             $categoriesFormation = $formation->getCategories();
-            foreach($categoriesFormation as $categorieFormation)
-            if(!$categories->contains($categorieFormation->getName())){
-                $categories[] = $categorieFormation->getName();
+            foreach($categoriesFormation as $categorieFormation) {
+                if(!$categories->contains($categorieFormation->getName())){
+                    $categories[] = $categorieFormation->getName();
+                }
             }
         }
         return $categories;
