ae735dbb1f5d355b6cc78e674e5241d9a4ec4d19
webby! <sritarbrt@gmail.com>
2026-02-05 17:48:19 +0100

    Ajout de la visibilité au constructeur

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -27,7 +27,7 @@ class FormationsController extends AbstractController {
      */
     private $categorieRepository;
 
-    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
         $this->formationRepository = $formationRepository;
         $this->categorieRepository = $categorieRepository;
     }
