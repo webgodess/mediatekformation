502f3b8be38bd117cd2534b967ad17d04e9bdaed
webby! <sritarbrt@gmail.com>
2026-02-16 22:29:10 +0100

    fix: rectification d'une erreur de chemin

--- src/Controller/PlaylistsController.php
+++ src/Controller/PlaylistsController.php
@@ -88,7 +88,7 @@ class PlaylistsController extends AbstractController {
         $playlist = $this->playlistRepository->find($id);
         $playlistCategories = $this->categorieRepository->findAllForOnePlaylist($id);
         $playlistFormations = $this->formationRepository->findAllForOnePlaylist($id);
-        return $this->render(self::RENDER_PATH, [
+        return $this->render("pages/playlist.html.twig", [
             'playlist' => $playlist,
             'playlistcategories' => $playlistCategories,
             'playlistformations' => $playlistFormations
