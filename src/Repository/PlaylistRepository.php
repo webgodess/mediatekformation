1f9883d8ceae39d80dbce10df851c3b3b8887833
webby! <sritarbrt@gmail.com>
2026-02-17 16:52:13 +0100

    feat: changement de nom de la fonction pour la rendre plus descriptive

--- src/Repository/PlaylistRepository.php
+++ src/Repository/PlaylistRepository.php
@@ -50,7 +50,7 @@ class PlaylistRepository extends ServiceEntityRepository
      * @param type $ordre
      * @return Playlist[]
      */
-    public function findAllOrderByNumber($ordre): array{
+    public function findAllOrderByNumberFormations($ordre): array{
         return $this->createQueryBuilder('p')
                 ->select('p, COUNT(f.id) AS HIDDEN nbFormations')
                 ->leftjoin('p.formations', 'f')
