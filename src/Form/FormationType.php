49cae6c3f74b635b6980f28ee07ce196f279e319
webby! <sritarbrt@gmail.com>
2026-02-20 20:55:30 +0100

    Ajout de formulaire

---
+++ src/Form/FormationType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Entity\Playlist;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('title')
+            ->add('description')
+            ->add('videoId')
+            ->add('date')
+            ->add('playlist', EntityType::class, [
+                'class' => Playlist::class,
+                'choice_label' => 'name',
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
